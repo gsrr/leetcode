7189631b420c751d5b72ff8c43728273c082ea7b
gsrr <jerrycheng1128@gmail.com>
2017-08-04 11:18:30 +0800

    update

--- hackerrank/Moody_Analytics_Women_in_Engineering_Hackathon/Trial_Balance.py
+++ hackerrank/Moody_Analytics_Women_in_Engineering_Hackathon/Trial_Balance.py
@@ -91,8 +91,40 @@ def solve3(n,m,d,c):
             midval = t
             minval = tmpval
     print midval
+    
+    maxA = [-10000] * (total + 1)
+    minA = [10000] * (total + 1)
+    maxB = [-10000] * (total + 1)
+    minB = [10000] * (total + 1)
+    maxA[0], minA[0], maxB[0], minB[0] = (0, 0, 0, 0)
+    for i in xrange(len(d)):
+        for t in xrange(total, d[i] - 1, -1):
+            maxA[t] = max(maxA[t], maxA[t - d[i]] + 1)
+            minA[t] = min(minA[t], minA[t - d[i]] + 1)
 
-    return (0, 0)
+    for i in xrange(len(c)):
+        for t in xrange(total, c[i] - 1, -1):
+            maxB[t] = max(maxB[t], maxB[t - c[i]] + 1)
+            minB[t] = min(minB[t], minB[t - c[i]] + 1)
+    
+    min_steps = m + n
+    min_t = 0
+    for t in xrange(midval + 1):
+        if maxA[t] >= 0 and minA[t] <= n and minB[minval-t] >= 0 and minB[minval - t] <= m: 
+            val = n - maxA[t] + minB[minval - t]
+            if val < min_steps:
+                min_steps = val
+                min_t = t
+                print "a"
+        if maxB[t] >= 0 and minB[t] <= m and minA[minval-t] >= 0 and minA[minval - t] <= n: 
+            val = m - maxB[t] + minA[minval - t]
+            if val < min_steps:
+                min_steps = val
+                min_t = t
+                print "b"
+
+    print min_t
+    return [total - 2 * midval, min_steps]
 
 if __name__ == "__main__":
     n, m = raw_input().strip().split(' ')
