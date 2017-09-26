e1e1449f3e9e99f405f00475adcfed3b3d230f19
gsrr <jerrycheng1128@gmail.com>
2017-09-27 02:34:16 +0800

    update #!/bin/python

---
+++ hackerrank/Left Rotation.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+import sys
+
+def leftRotation(a, d):
+    # Complete this function
+    return a[d:] + a[:d]
+
+if __name__ == "__main__":
+    n, d = raw_input().strip().split(' ')
+    n, d = [int(n), int(d)]
+    a = map(int, raw_input().strip().split(' '))
+    result = leftRotation(a, d)
+    print " ".join(map(str, result))
+
+
+
