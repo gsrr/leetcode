7a2c75e35a2ac4656de1343f3e31beb0fc1c4241
Jerry.Cheng <jerry.cheng@infortrend.com>
2017-11-10 14:18:19 +0800

    modify 309

--- leetcode/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ leetcode/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,13 +1,61 @@
+def ans_3(prices):
+    '''
+    Dynamic Programming (from selling stock)
+    
+    Time Complexity : O(n^2)
+    Space Complexity : O(n)
+    '''
+    if len(prices) == 0:
+        return 0
+    dp = [0] * (len(prices) + 2)
+    for i in xrange(len(prices) - 1, -1, -1):
+        dp[i] = dp[i + 1]
+        for j in xrange(i, len(prices)):
+            dp[i] = max(dp[i], prices[j] - prices[i] + dp[j + 2])
+    print dp
+    return dp[0]
+
+def ans_2(prices):
+    '''
+    Dynamic Programming (from buying stock)
+    
+    Time Complexity : O(n^2)
+    Space Complexity : O(n)
+    '''
+    if len(prices) == 0:
+        return 0
+    dp = [0] * (len(prices) + 2)
+    for i in xrange(1, len(prices)):
+        di = i + 2
+        dp[di] = dp[di - 1]
+        for j in xrange(i, -1, -1):
+            dj = j + 2
+            dp[di] = max(dp[di], prices[i] - prices[j] + dp[dj - 2])
+    
+    return dp[-1]
+
+def ans_1(prices):
+    '''
+    Dynamic Programming (from buying stock)
+    
+    Time Complexity : O(n^2)
+    Space Complexity : O(n)
+    '''
+    if len(prices) == 0:
+        return 0
+    dp = [0] * (len(prices) + 2)
+    for i in xrange(1, len(prices)):
+        dp[i] = dp[i - 1]
+        for j in xrange(i, -1, -1):
+            dp[i] = max(dp[i], prices[i] - prices[j] + dp[j - 2])
+    
+    return dp[-3]
+
 class Solution(object):
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        dp = [0] * (len(prices) + 2)
-        dp[-1] = 0
-        for i in xrange(len(prices) - 2, -1, -1):
-            dp[i] = dp[i + 1]
-            for j in xrange(i + 1, len(prices)):
-                dp[i] = max(dp[i], prices[j] - prices[i] + dp[j + 2])
-        return dp[0]
+        return ans_3(prices)
+        
