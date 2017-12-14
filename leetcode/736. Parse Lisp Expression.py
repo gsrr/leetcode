b18ad81d12c05ff0035cc1e002dc41abb5b4ebeb
gsrr <jerrycheng1128@gmail.com>
2017-12-15 07:00:29 +0800

    add 736

--- leetcode/736. Parse Lisp Expression.py
+++ leetcode/736. Parse Lisp Expression.py
@@ -36,23 +36,23 @@ def eval2(tokens, dic):
         return 0
 
     token = tokens[idx]
-    tdic = copy.deepcopy(dic)
     if token == "add":
         idx += 1
-        v1 = eval2(tokens, tdic) 
-        v2 = eval2(tokens, tdic)
+        v1 = eval2(tokens, dic) 
+        v2 = eval2(tokens, dic)
         idx += 1
         return v1 + v2
     elif token == "mult":
         idx += 1
-        v1 = eval2(tokens, tdic) 
-        v2 = eval2(tokens, tdic)
+        v1 = eval2(tokens, dic) 
+        v2 = eval2(tokens, dic)
         idx += 1
         return v1 * v2
     elif token == "let":
+        tdic = copy.deepcopy(dic)
         idx += 1
         v1 = tokens[idx]
-        while v1 != "(" and v1 != ")":
+        while v1 != "(":
             if tokens[idx + 1] == ")":
                 idx += 1
                 return str2int(v1, tdic)
@@ -64,25 +64,124 @@ def eval2(tokens, dic):
         return eval2(tokens, tdic)
     elif token == "(":
         idx += 1
-        return eval2(tokens, tdic)
+        return eval2(tokens, dic)
     elif token == ")":
         idx += 1
-        return eval2(tokens, tdic)
+        return eval2(tokens, dic)
     else:
         idx += 1
-        return str2int(token, tdic)
+        return str2int(token, dic)
+
+def ans_1(expression):
+    global idx
+    idx = 0
+    ret = 0
+    tokens = get_tokens(expression)
+    return eval2(tokens, {})
+
+
+def is_int(token):
+    try:
+        int(token)
+        return True
+    except:
+        return False
+
+class Context:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.idx = 0
+        self.state = StartState()
         
+    def get_token(self):
+        return self.tokens[self.idx]
+    
+    def do(self, dic):
+        return self.state.do(self, dic)
+    
+class State:
+    def do(self, cxt, dic):
+        pass
+
+class StartState(State):
+    def do(self, cxt, dic):
+        token = cxt.get_token()
+        if token == "(":
+            cxt.idx += 1
+        elif token == "add":
+            cxt.state = AddState()
+            cxt.idx += 1
+        elif token == "mult":
+            cxt.state = MultState()
+            cxt.idx += 1
+        elif token == "let":
+            cxt.state = LetState()
+            cxt.idx += 1
+        elif token == ")":
+            cxt.idx += 1
+            return None
+        elif is_int(token) == True:
+            cxt.idx += 1
+            return int(token)
+        else:
+            cxt.idx += 1
+            return dic[token]
+        return cxt.do(dic)
+    
+class AddState(State):
+    def do(self, cxt, dic):
+        token = cxt.get_token()
+        cxt.state = StartState()
+        v1 = cxt.do(dic)
+        cxt.state = StartState()
+        v2 = cxt.do(dic)
+        cxt.idx += 1
+        return v1 + v2
+
+class MultState(State):
+    def do(self, cxt, dic):
+        token = cxt.get_token()
+        cxt.state = StartState()
+        v1 = cxt.do(dic)
+        cxt.state = StartState()
+        v2 = cxt.do(dic)
+        cxt.idx += 1
+        return v1 * v2
+
+class LetState(State):
+    def do(self, cxt, dic):
+        tdic = copy.deepcopy(dic)
+        while True:
+            token = cxt.get_token()
+            if token == "(":
+                cxt.state = StartState()
+                val = cxt.do(tdic)
+                cxt.idx += 1
+                return val
+            if token == ")":
+                pass
+            v1 = token
+            cxt.idx += 1
+            cxt.state = StartState()
+            v2 = cxt.do(tdic)
+            if v2 == None:
+                if is_int(v1) == True:
+                    return int(v1)
+                return tdic[v1]
+            tdic[v1] = int(v2)
+            
+        
+        
+def ans_2(expression):
+    tokens = get_tokens(expression)
+    cxt = Context(tokens)
+    dic = {}
+    return cxt.do(dic)
+    
 class Solution(object):
     def evaluate(self, expression):
         """
         :type expression: str
         :rtype: int
         """
-        global idx
-        idx = 0
-        ret = 0
-        tokens = get_tokens(expression)
-        #print tokens
-        while idx < len(tokens):
-            ret += eval2(tokens, {})
-        return ret
+        return ans_2(expression)        
