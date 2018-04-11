ebb1376612294779f6dc2c94d2a97ec035c00d9d
gsrr <jerrycheng1128@gmail.com>
2018-04-12 07:17:51 +0800

    add 813:accept

--- leetcode/813. Largest Sum of Averages.py
+++ leetcode/813. Largest Sum of Averages.py
@@ -34,6 +34,7 @@ def ans1(A, K):
 def ans2(A, K):
     '''
     Method : Greedy + Dynamic programming
+    Result : Wrong answer, it can not be solved by greedy method.
     Time complexity : O(k * n)
     Space complexity : O(1)
     '''
@@ -70,6 +71,47 @@ def ans2(A, K):
         summ += dp[key]
     return summ
 
+
+def recur_util3(hist, A, K, curr_sum):
+    if len(A) <= K:
+        return curr_sum + sum(A)
+    
+    if K == 1:
+        return curr_sum + sum(A)/float(len(A)) 
+    
+    if hist.has_key((len(A), K)) == True:
+        return curr_sum + hist[(len(A), K)]
+    max_sum = 0
+    tmp_sum = 0
+    for i in xrange(len(A)):
+        tmp_sum += A[i]
+        max_sum = max(max_sum, recur_util(hist, A[i + 1:], K - 1, tmp_sum / float(i + 1)))
+    hist[(len(A), K)] = max_sum
+    return curr_sum + max_sum
+
+def ans3(A, K):
+    '''
+    Method : Recursive with cache
+    Result : Accept
+    Time complexity : 
+    Space complexity : O(K * N) and function stack : O(n)
+    '''
+    if len(A) <= K:
+        return sum(A)
+    
+    if K == 1:
+        return sum(A)/float(len(A))
+    
+    hist = {}
+    max_sum = 0
+    summ = 0
+    for i in xrange(len(A)):
+        summ += A[i]
+        max_sum = max(max_sum, recur_util(hist, A[i + 1:], K - 1, summ / float(i + 1)))
+        #print hist
+    return max_sum    
+
+
 class Solution(object):
     def largestSumOfAverages(self, A, K):
         """
@@ -78,4 +120,5 @@ class Solution(object):
         :rtype: float
         """
         #return ans1(A, K)
-        return ans2(A, K)
+        #return ans2(A, K)
+        return ans3(A, K)
