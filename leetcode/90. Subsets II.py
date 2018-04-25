bb32b0d96dec032fa7b5b0fa8fa0acb0334ffb6e
Jerry.Cheng <jerry.cheng@infortrend.com>
2018-04-26 05:35:54 +0800

    update 90

--- leetcode/90. Subsets II.py
+++ leetcode/90. Subsets II.py
@@ -1,16 +1,51 @@
 import itertools
 
+
+def ans3(nums):
+    nums.sort()
+    ret = {}
+    for i in xrange(len(nums) + 1):
+        it = itertools.combinations(nums, i)
+        for line in it:
+            ret[line] = True
+    return [ list(x) for x in ret.keys()]
+
+import copy
+
+def ans1(nums):
+    nums.sort()
+    ret = [[]]
+    hist = {}
+    for i in xrange(len(nums)):
+        elems = copy.deepcopy(ret) # slowly
+        for e in elems:
+            e.append(nums[i])
+            if hist.has_key(tuple(e)) == False:
+                ret.append(list(e))
+                hist[tuple(e)] = True
+    return ret
+
+def ans2(nums):
+    '''
+    Remove copy method
+    '''
+    nums.sort()
+    ret = [[]]
+    hist = {}
+    for i in xrange(len(nums)):
+        n = len(ret)
+        for j in xrange(n):
+            e = list(ret[j])
+            e.append(nums[i])
+            if hist.has_key(tuple(e)) == False:
+                ret.append(e)
+                hist[tuple(e)] = True
+    return ret
+
 class Solution(object):
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()
-        ret = {}
-        for i in xrange(len(nums) + 1):
-            it = itertools.combinations(nums, i)
-            for line in it:
-                ret[line] = True
-        return [ list(x) for x in ret.keys()]
-        
+        return ans2(nums)    
