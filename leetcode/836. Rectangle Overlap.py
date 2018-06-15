963cdac8d57094e28d4c10c54107cc231942ad52
Jerry.Cheng <jerry.cheng@infortrend.com>
2018-06-16 07:38:20 +0800

    add 836. Rectangle Overlap.py

--- leetcode/836. Rectangle Overlap.py
+++ leetcode/836. Rectangle Overlap.py
@@ -40,6 +40,20 @@ def ans(rec1, rec2):
     else:
         return check_rect(rec2, rec1)
 
+def ans_new(x1, y1, x2, y2, x3, y3, x4, y4):
+    if x3 >= x2 or x1 >= x4:
+        return 0
+    if y3 >= y2 or y1 >= y4:
+        return 0
+    
+    return 1
+    '''
+    xarr = [x1, x2, x3, x4]
+    yarr = [y1, y2, y3, y4]
+    xarr.sort()
+    yarr.sort()
+    return (xarr[2] - xarr[1]) * (yarr[2] - yarr[1])
+    '''
 rec1 = [0,0,2,2]
 rec2 = [1,1,3,3]
 print ans(rec1, rec2)
