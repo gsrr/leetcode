761345bf8b9b217fa7bd8fed00a6910931cc5de3
Jerry.Cheng <jerry.cheng@infortrend.com>
2018-07-28 08:34:20 +0800

    update

--- hackerrank/week_of_code_19/Fix the Cycles.py
+++ hackerrank/week_of_code_19/Fix the Cycles.py
@@ -3,7 +3,7 @@
 import collections
 
 def bellman(graph, s):
-    print graph, s
+    #print graph, s
     n = 4
     dp = [float("inf")] * n 
     dp[s] = 0
@@ -33,8 +33,18 @@ def bellman(graph, s):
                 neg_cycle = True
     
     if neg_cycle == False:
-        return 0
+        return []
     
+    for i in xrange(n):
+        x = prev[x]
+        
+    cycle = [x]
+    v = prev[x]
+    while v != x:
+        cycle.append(v)
+        v = prev[v]
+    cycle.append(v)
+    return cycle[::-1] 
     
 pval = 0
 
@@ -76,6 +86,51 @@ def ans(arr):
     #print pval
     return pval
 
+def ans_bellman(arr):
+    edges = [(3, 0), (0, 1), (1, 2), (2, 3), (0, 2), (1, 3)]
+    #print arr
+    graph = collections.defaultdict(list)
+    
+    for i in xrange(len(edges)):
+        u = edges[i][0]
+        v = edges[i][1]
+        cost = arr[i]
+        graph[u].append([v, cost])
+        
+    ret = {}
+    cycle = bellman(graph, 0)
+    while len(cycle) != 0:
+        sc = 0
+        imin = (0, 0)
+        tmin = 0x7fffffff
+        for i in xrange(1, len(cycle)):
+            u = cycle[i - 1]
+            v = cycle[i]
+            for j in xrange(len(graph[u])):
+                if graph[u][j][0] == v:
+                    sc += graph[u][j][1]
+                    if graph[u][j][1] < tmin:
+                        tmin = graph[u][j][1]
+                        imin = (u, v)
+        #print cycle, sc, imin, tmin
+        if ret.has_key(imin) == False:
+            ret[imin] = 0
+        
+        ret[imin] += (-1 * sc)
+        
+        u = imin[0]
+        v = imin[1]
+        for j in xrange(len(graph[u])):
+            if graph[u][j][0] == v:
+                graph[u][j][1] += (-1 * sc)
+        cycle = bellman(graph, 0)
+    #print ret
+    gmax = 0
+    for key in ret.keys():
+        if ret[key] > gmax:
+            gmax = ret[key]
+    return gmax
+
 if __name__ == "__main__":
     arr = map(int, raw_input().strip().split())
-    print ans(arr)
+    print ans_bellman(arr)
