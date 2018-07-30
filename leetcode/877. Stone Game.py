24986fb65507777902a887a66f877710d4df74be
Jerry.Cheng <jerry.cheng@infortrend.com>
2018-07-30 11:39:34 +0800

    update leetcode 877

--- leetcode/877. Stone Game.py
+++ leetcode/877. Stone Game.py
@@ -1,36 +1,57 @@
-
-global sval
-
-def recur_util(piles, turn, aval):
-    global sval
+ist = {}
+def recur_util(piles, turn):
+    global ghist
+    key = "%d:%s"%(turn, str(piles))
+    if ghist.has_key(key):
+        return ghist[key]
+    
+    
     if len(piles) == 1:
         return piles[0]
 
-    if aval > (sval // 2):
-        return True
-    
     mval = 0
     if turn % 2 == 1:
-        if recur_util(piles[1:], 1 - turn, aval):
-            return True
-        if recur_util(piles[:-1], 1 - turn, aval):
-            return True
+        mval = max(recur_util(piles[1:], 1 - turn), recur_util(piles[:-1], 1 - turn))
     else:
-        if recur_util(piles[1:], 1 - turn, piles[0] + aval):
-            return True
-        if recur_util(piles[:-1], 1 - turn, piles[-1] + aval):
-            return True
-    return False
+        mval = max(piles[0] + recur_util(piles[1:], 1 - turn), piles[-1] + recur_util(piles[:-1], 1 - turn))
+    ghist[key] = mval
+    return mval
 
 def ans(piles):
-    global sval
+    global ghist
+    ghist = {}
     sval = sum(piles)
     
-    if recur_util(piles[1:], 1, piles[0]):
-        return True
-    if recur_util(piles[:-1], 1, piles[-1]):
-        return True
-    return False
+    aval = max(piles[0] + recur_util(piles[1:], 1), piles[-1] + recur_util(piles[:-1], 1))
+    return aval > (sval // 2)
+
+def minMax(piles, s, e, turn):
+    global ghist
+    
+    if s == e:
+        return 0
+    
+    key = (s, e)
+    if ghist.has_key(key):
+        return ghist[key]
+    
+    rval = 0
+    if turn == 0:
+        rval = max(piles[s] + minMax(piles, s + 1, e, 1 - turn), piles[e - 1] + minMax(piles, s, e - 1, 1 - turn))
+    else:
+        rval = max(minMax(piles, s + 1, e, 1 - turn), minMax(piles, s, e - 1, 1 - turn))
+    ghist[key] = rval
+    return ghist[key]
+    
+def ans1(piles):
+    global ghist
+    ghist = {}
+    
+    sval = sum(piles)
+    s = 0
+    e = len(piles)
+    aval = max(piles[0] + minMax(piles, s + 1, e, 1), piles[-1] + minMax(piles, s, e - 1, 1))
+    return aval > (sval // 2)
 
 class Solution(object):
     def stoneGame(self, piles):
@@ -38,5 +59,5 @@ class Solution(object):
         :type piles: List[int]
         :rtype: bool
         """
-        return ans(piles)
+        return ans1(piles)
         
