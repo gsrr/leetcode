a0629037bda7089f1b44c7fc718588ddcf4e9393
Jerry.Cheng <jerry.cheng@infortrend.com>
2018-07-31 16:43:52 +0800

    add 877. Stone Game.py - Dynamic programming

--- leetcode/877. Stone Game.py
+++ leetcode/877. Stone Game.py
@@ -1,4 +1,4 @@
-ist = {}
+ghist = {}
 def recur_util(piles, turn):
     global ghist
     key = "%d:%s"%(turn, str(piles))
@@ -25,7 +25,7 @@ def ans(piles):
     aval = max(piles[0] + recur_util(piles[1:], 1), piles[-1] + recur_util(piles[:-1], 1))
     return aval > (sval // 2)
 
-def minMax(piles, s, e, turn):
+def minMax1(piles, s, e, turn):
     global ghist
     
     if s == e:
@@ -37,9 +37,9 @@ def minMax(piles, s, e, turn):
     
     rval = 0
     if turn == 0:
-        rval = max(piles[s] + minMax(piles, s + 1, e, 1 - turn), piles[e - 1] + minMax(piles, s, e - 1, 1 - turn))
+        rval = max(piles[s] + minMax1(piles, s + 1, e, 1 - turn), piles[e - 1] + minMax1(piles, s, e - 1, 1 - turn))
     else:
-        rval = max(minMax(piles, s + 1, e, 1 - turn), minMax(piles, s, e - 1, 1 - turn))
+        rval = max(minMax1(piles, s + 1, e, 1 - turn), minMax1(piles, s, e - 1, 1 - turn))
     ghist[key] = rval
     return ghist[key]
     
@@ -50,14 +50,89 @@ def ans1(piles):
     sval = sum(piles)
     s = 0
     e = len(piles)
-    aval = max(piles[0] + minMax(piles, s + 1, e, 1), piles[-1] + minMax(piles, s, e - 1, 1))
+    aval = max(piles[0] + minMax1(piles, s + 1, e, 1), piles[-1] + minMax1(piles, s, e - 1, 1))
     return aval > (sval // 2)
 
+def minMax2(piles, s, e, turn):
+    global ghist
+    
+    if s == e:
+        return 0
+    
+    key = (s, e, turn)
+    if ghist.has_key(key):
+        return ghist[key]
+    
+    rval = 0
+    if turn == 0:
+        rval = max(piles[s] + minMax2(piles, s + 1, e, 1 - turn), piles[e - 1] + minMax2(piles, s, e - 1, 1 - turn))
+    else:
+        rval = max(minMax2(piles, s + 1, e, 1 - turn), minMax2(piles, s, e - 1, 1 - turn))
+    ghist[key] = rval
+    return ghist[key]
+    
+def ans2(piles):
+    global ghist
+    ghist = {}
+    
+    sval = sum(piles)
+    s = 0
+    e = len(piles)
+    aval = max(piles[0] + minMax2(piles, s + 1, e, 1), piles[-1] + minMax2(piles, s, e - 1, 1))
+    return aval > (sval // 2)
+
+def minMax3(piles, s, e, turn):
+    global ghist
+    
+    if s == e:
+        return 0
+    
+    key = (s, e, turn)
+    if ghist.has_key(key):
+        return ghist[key]
+    
+    rval = 0
+    if turn == 0:
+        rval = max(piles[s] + minMax3(piles, s + 1, e, 1 - turn), piles[e - 1] + minMax3(piles, s, e - 1, 1 - turn))
+    else:
+        rval = min(-piles[s] + minMax3(piles, s + 1, e, 1 - turn), -piles[e - 1] + minMax3(piles, s, e - 1, 1 - turn))
+    ghist[key] = rval
+    return ghist[key]
+    
+def ans3(piles):
+    global ghist
+    ghist = {}
+    
+    sval = sum(piles)
+    s = 0
+    e = len(piles)
+    aval = max(piles[0] + minMax3(piles, s + 1, e, 1), piles[-1] + minMax3(piles, s, e - 1, 1))
+    return aval > 0
+
+def ans4(piles):
+    # piles.length is even.
+    
+    dp = [[0] * (len(piles) + 1) for _ in xrange(len(piles) + 1)]
+    
+    for i in xrange(len(dp)):
+        dp[i][i] = 0
+        
+    for k in xrange(1, len(dp)):
+        for i in xrange(len(dp) - k):
+            #print i, i + k
+            if k % 2 == 0: # A turn
+                dp[i][i + k] = max( piles[i] + dp[i + 1][i + k], piles[i + k - 1] + dp[i][i + k - 1] )
+            else:
+                dp[i][i + k] = min( -piles[i] + dp[i + 1][i + k], -piles[i + k - 1] + dp[i][i + k - 1] )
+    
+    #print dp
+    return dp[0][-1] > 0
+                
 class Solution(object):
     def stoneGame(self, piles):
         """
         :type piles: List[int]
         :rtype: bool
         """
-        return ans1(piles)
+        return ans4(piles)
         
